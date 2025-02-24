e2cc0883de25996282be3ede2e96a54187068765
Neema Deshwal <neemadeshwal.py@gmail.com>
2025-02-24 21:13:37 +0530

    changes

--- server/src/utils/redis/redis.ts
+++ server/src/utils/redis/redis.ts
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-import Redis from "ioredis"
-import { REDIS_URL } from "../constants"
-
-export const redis=new Redis(  REDIS_URL 
-)
-=======
 import Redis from "ioredis";
-import {
-  REDIS_HOST,
-  REDIS_PASSWORD,
-  REDIS_PORT,
-  REDIS_URL,
-} from "../constants";
+import { REDIS_URL } from "../constants";
 
-export const redis = new Redis(REDIS_URL!);
->>>>>>> old-repo/master
+export const redis = new Redis(REDIS_URL);
